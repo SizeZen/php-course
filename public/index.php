c5aedc9efc1ed9d53781329a45151fc45f03d0e2
SizeZen <ivadimdobrydnyk@gmail.com>
2016-04-07 09:33:17 +0300

    Added PhpRenderer

--- public/index.php
+++ public/index.php
@@ -1,10 +1,14 @@
 <?php
+use Slim\Views\PhpRenderer;
 use \Psr\Http\Message\ServerRequestInterface as Request;
 use \Psr\Http\Message\ResponseInterface as Response;
 
 require '../vendor/autoload.php';
 
-$app = new \Slim\App;
+$app = new Slim\App;
+$container = $app->getContainer();
+$container['renderer'] = new PhpRenderer("./views/");
+
 $app->get('/hello/{name}', function (Request $request, Response $response) {
     $name = $request->getAttribute('name');
     $response->getBody()->write("Hello, $name");
@@ -12,3 +16,5 @@ $app->get('/hello/{name}', function (Request $request, Response $response) {
     return $response;
 });
 $app->run();
+
+
