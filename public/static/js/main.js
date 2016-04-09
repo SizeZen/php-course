e73e06981c673113d008008ec2508806ef4d8ed9
SizeZen <ivadimdobrydnyk@gmail.com>
2016-04-09 22:39:53 +0300

    Added static files

---
+++ public/static/js/main.js
@@ -0,0 +1,69 @@
+$(document).ready(function(){
+    $('#addUser').click(function(e){
+        var data = {
+            "username": $('#username').val(),
+            "userlogin": $('#userlogin').val(),
+            "usertype": $('#usertype').val()
+        };
+        data = JSON.stringify(data);
+        $.post('/api/addUser', data)
+        .done(function(data, status, xhr) {
+            var responseInfo = $('#responseinfo');
+            responseInfo.removeClass();
+            responseInfo.addClass('text-success');
+            responseInfo.html("Запит виконано успішно");
+            location.reload();
+        })
+        .fail(function(data, status, xhr) {
+            var responseInfo = $('#responseinfo');
+            responseInfo.removeClass();
+            responseInfo.addClass('text-danger');
+            responseInfo.html("Не правильні дані");
+        });    
+    });
+    $('.fireUser').click(function(e){
+        var data = {
+            "id": $(e.currentTarget).parent().parent().get(0).firstElementChild.innerHTML
+        };
+        console.log(data);
+
+        data = JSON.stringify(data);
+        $.post('/api/fireUser', data)
+        .done(function(data, status, xhr) {
+            location.reload();
+        })
+    });
+    $('.acceptUser').click(function(e){
+        var data = {
+            "id": $(e.currentTarget).parent().parent().get(0).firstElementChild.innerHTML
+        };
+        console.log(data);
+
+        data = JSON.stringify(data);
+        $.post('/api/acceptUser', data)
+        .done(function(data, status, xhr) {
+            location.reload();
+        })
+    });
+    $('.updateUser').click(function(e){
+        var id = $(e.currentTarget).parent().parent().get(0).children[0].innerHTML;
+        var name = $(e.currentTarget).parent().parent().get(0).children[1].innerHTML;
+
+        console.log(id)
+        console.log(name)
+        $('#UpdateWorker .userid').val(id)
+        $('#UpdateWorker .username').val(name)
+    });
+    $('#updateUser').click(function(e){
+        var data = {
+            "username": $('.username').val(),
+            "userid": $('.userid').val()
+        };
+
+        data = JSON.stringify(data);
+        $.post('/api/updateUser', data)
+        .done(function(data, status, xhr) {
+            location.reload();
+        })
+    });
+});
