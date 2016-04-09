57c2afb6f2f4a2718a29f0df61a589a267eee635
SizeZen <ivadimdobrydnyk@gmail.com>
2016-04-09 22:37:25 +0300

    Added js tasks in gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,19 @@
 var gulp = require('gulp');
 var concat = require('gulp-concat');
 
-gulp.task('default', function () {
+gulp.task('default', ['concatCss', 'replaceJs', 'replaceFonts']);
+
+gulp.task('concatCss', function () {
   return gulp.src('bower_components/bootstrap/dist/css/bootstrap.css')
     .pipe(concat('style.css'))
     .pipe(gulp.dest('public/static'));
+});
+
+gulp.task('replaceJs', function () {
+  return gulp.src(['bower_components/bootstrap/dist/js/bootstrap.js', 'bower_components/jquery/dist/jquery.js'])
+    .pipe(gulp.dest('public/static/js'));
+});
+gulp.task('replaceFonts', function () {
+  return gulp.src('bower_components/bootstrap/dist/fonts/*')
+    .pipe(gulp.dest('public/fonts'));
 });
